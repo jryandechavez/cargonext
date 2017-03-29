c1a33736b44ae9d7b38789808d82dfc9f1aee3d5
root <jeff@ossph.com>
2017-03-29 02:27:15 +0000

    modified:   cargonext/config/desktop.py 	new file:   cargonext/forwarding/doctype/consol/__init__.py 	new file:   cargonext/forwarding/doctype/consol/consol.js 	new file:   cargonext/forwarding/doctype/consol/consol.json 	new file:   cargonext/forwarding/doctype/consol/consol.py 	new file:   cargonext/forwarding/doctype/consol/test_consol.py 	new file:   cargonext/forwarding/doctype/routing/__init__.py 	new file:   cargonext/forwarding/doctype/routing/routing.json 	new file:   cargonext/forwarding/doctype/routing/routing.py 	modified:   cargonext/forwarding/doctype/shipment/shipment.json 	modified:   requirements.txt

--- cargonext/config/desktop.py
+++ cargonext/config/desktop.py
@@ -7,42 +7,42 @@ def get_data():
 		{
 			"module_name": "CargoNext",
 			"color": "grey",
-			"icon": "icon-truck",
+			"icon": "fa fa-truck",
 			"type": "module",
 			"label": _("CargoNext")
 		},
                 {
                         "module_name": "Forwarding",	
                         "color": "green",
-                        "icon": "icon-suitcase",
+                        "icon": "fa fa-ship",
                         "type": "module",
                         "label": _("Forwarding")
                 },
                 {
                         "module_name": "Haulage",
                         "color": "blue",
-                        "icon": "icon-truck",
+                        "icon": "fa fa-truck",
                         "type": "module", 
                         "label": _("Haulage")
                 },
                 {
                         "module_name": "Customs",
                         "color": "red",
-                        "icon": "icon-file-text",
+                        "icon": "fa fa-certificate",
                         "type": "module",
                         "label": _("Customs")
                 },
                 {
                         "module_name": "Warehousing",
                         "color": "brown",
-                        "icon": "icon-qrcode",
+                        "icon": "fa fa-qrcode",
                         "type": "module",
                         "label": _("Warehousing")
                 },
                 {
                         "module_name": "General Setup",
                         "color": "brown",
-                        "icon": "icon-wrench",
+                        "icon": "fa fa-wrench",
                         "type": "module",
                         "label": _("General Setup")
                 }                                                                                                           
